cad893e95a2d15c9b1a01ac5b371dee2351412c6
Yogoda <joelfivat@hotmail.com>
2020-08-25 20:06:23 +0200

    update

--- scripts/zone_loader.gd
+++ scripts/zone_loader.gd
@@ -24,6 +24,7 @@ var request_exit = false
 var exit_lock:Mutex = Mutex.new()
 
 export var zone_path:String
+export var show_debug = false
 
 var current_zones = {} #zones the player is in (1-2)
 var current_zone #zone the player is in (arbitrary)
@@ -42,6 +43,11 @@ func _ready():
 	#start background loading process
 	# warning-ignore:return_value_discarded
 	loading_process.start(self, "_loading_process")
+	
+func _print(text):
+	
+	if show_debug:
+		print(text)
 	
 func get_zone_path(zone_id):
 	return str(zone_path, zone_id, ".tscn")
@@ -62,7 +68,7 @@ func get_connected_zones(zone_id):
 
 func _on_zone_entered(zone_id):
 	
-	print("zone ", zone_id, " entered")
+	_print(str("zone ", zone_id, " entered"))
 	
 	current_zone = zone_id
 	current_zones[zone_id] = zone_id
@@ -74,11 +80,11 @@ func _on_zone_entered(zone_id):
 	
 	#load connected zones
 	for connected_zone in zone.get_overlapping_areas():
-		print("load connected zone ", connected_zone.name)
+		_print(str("load connected zone ", connected_zone.name))
 		request_instance(connected_zone.name)
 	
 func _on_zone_exited(zone_id):
-	print("zone ", zone_id, " exited")
+	_print(str("zone ", zone_id, " exited"))
 	
 	current_zones.erase(zone_id)
 	
@@ -123,7 +129,7 @@ func attach_zone(zone_id, instance):
 	if not get_zone(zone_id):
 	
 		get_node(zone_id).call_deferred("add_child", instance)
-		print("zone", zone_id, " attached")
+		_print(str("zone", zone_id, " attached"))
 		
 #remove zone from the scene tree
 func detach_zone(zone_id):
@@ -133,7 +139,7 @@ func detach_zone(zone_id):
 	for child in get_node(zone_id).get_children():
 		if not child is CollisionShape and not child is CollisionPolygon:
 			zone.remove_child(child)
-			print("zone ", zone_id, " detached")
+			_print(str("zone ", zone_id, " detached"))
 			break
 		
 func post_queue_action(action_id, zone_id, priority = false):
@@ -149,7 +155,7 @@ func post_queue_action(action_id, zone_id, priority = false):
 				return
 				
 			action_queue.erase(action)
-			print("action removed ", action.action_id, " ", action.zone_id)
+			_print(str("action removed ", action.action_id, " ", action.zone_id))
 
 	if priority:
 		action_queue.push_front({action_id = action_id, zone_id = zone_id})
@@ -162,7 +168,7 @@ func post_queue_action(action_id, zone_id, priority = false):
 	
 func _process_load_action(zone_id):
 	
-	print("process load zone ", zone_id)
+	_print(str("process load zone ", zone_id))
 
 	#using deferred so that the signal is processed by the main thread
 	call_deferred("emit_signal", "sig_zone_loading_started", zone_id)
@@ -176,7 +182,7 @@ func _process_load_action(zone_id):
 		
 		exit_lock.lock()
 		if request_exit:
-			print("exiting, loading arborted")
+			_print("exiting, loading arborted")
 			break
 		exit_lock.unlock()
 
@@ -188,12 +194,12 @@ func _process_load_action(zone_id):
 			resource = loader.get_resource()
 			loaded_zones_lock.lock()
 			loaded_zones[zone_id] = {resource = resource, instance = null}
-			print("zone ", zone_id, " loaded in ", (OS.get_ticks_msec() - ts) / 1000.0, "s")
+			_print(str("zone ", zone_id, " loaded in ", (OS.get_ticks_msec() - ts) / 1000.0, "s"))
 			loaded_zones_lock.unlock()
 			break
 			
 		elif err != OK:
-			print("error loading resource: ", zone_path , " ",  str(err))
+			_print(str("error loading resource: ", zone_path , " ",  str(err)))
 			break
 			
 	#using deferred so that the signal is processed by the main thread
@@ -201,7 +207,7 @@ func _process_load_action(zone_id):
 	
 func _process_unload_action(zone_id):
 	
-	print("process unload zone ", zone_id)
+	_print(str("process unload zone ", zone_id))
 	
 	loaded_zones_lock.lock()
 
@@ -214,10 +220,10 @@ func _process_unload_action(zone_id):
 			
 		loaded_zones.erase(zone_id)
 
-		print("zone ", zone_id, " unloaded")
+		_print(str("zone ", zone_id, " unloaded"))
 
 	else:
-		print("zone ", zone_id, " already unloaded")
+		_print(str("zone ", zone_id, " already unloaded"))
 		
 	loaded_zones_lock.unlock()
 
@@ -225,7 +231,7 @@ func _process_instance_action(zone_id):
 
 	var zone_instance
 
-	print("process instance zone ", zone_id)
+	_print(str("process instance zone ", zone_id))
 
 	loaded_zones_lock.lock()
 	var zone_loaded = loaded_zones.has(zone_id)
@@ -236,7 +242,7 @@ func _process_instance_action(zone_id):
 		loaded_zones_lock.lock()
 
 		if loaded_zones[zone_id] == null:
-			print("ERROR: ZONE LOADED IS NULL")
+			_print("ERROR: ZONE LOADED IS NULL")
 			loaded_zones_lock.unlock()
 			return
 		else:
@@ -245,25 +251,21 @@ func _process_instance_action(zone_id):
 
 		if zone_instance != null:
 
-			print("zone ", zone_id, " already instanced")
+			_print(str("zone ", zone_id, " already instanced"))
 			call_deferred("emit_signal", "sig_zone_instanced", zone_id, zone_instance)
 			return
 
 	#zone not loaded, proceed to loading
 	else:
 
-		print("instance: load zone ", zone_id)
+		_print(str("instance: load zone ", zone_id))
 		_process_load_action(zone_id)
-
-#	print("A")
 
 	exit_lock.lock()
 	if request_exit:
-		print("exiting, loading arborted")
+		_print("exiting, loading arborted")
 		return
 	exit_lock.unlock()
-
-#	print("B")
 
 	var resource
 
@@ -272,27 +274,19 @@ func _process_instance_action(zone_id):
 	resource = loaded_zones[zone_id].resource
 	loaded_zones_lock.unlock()
 
-#	print("C")
-
 	var ts = OS.get_ticks_msec()
 	zone_instance = resource.instance()
-	print("zone ", zone_id, " instanced in ", (OS.get_ticks_msec() - ts) / 1000.0, "s")
-
-#	print("D")
+	_print(str("zone ", zone_id, " instanced in ", (OS.get_ticks_msec() - ts) / 1000.0, "s"))
 
 	loaded_zones_lock.lock()
 	loaded_zones[zone_id].instance = zone_instance
 	loaded_zones_lock.unlock()
 
-#	print("E")
-
 	call_deferred("emit_signal", "sig_zone_instanced", zone_id, zone_instance)
-
-#	print("F")
 
 func _process_free_instance_action(zone_id):
 	
-	print("free zone")
+	_print("free zone")
 	
 	var zone_instance
 
@@ -308,23 +302,19 @@ func _process_free_instance_action(zone_id):
 
 	loaded_zones_lock.unlock()
 	
-	#create a new fresh instance for when player comes back into the zone
-	#we cannot keep the instance because of the error "_body_enter_tree: Condition "!E" is true"
-	_process_instance_action(zone_id)
+	#workaround for 3.2.2, fixed in 3.2.3 (pull request #41469)
+#	_process_instance_action(zone_id)
 	
 # warning-ignore:unused_argument
 func _loading_process(dummy):
 
-	print("loading process started")
+	_print("loading process started")
 	
 	while true:
 	
-#		print("loading thread ", OS.get_ticks_msec(), " ", OS.get_ticks_usec(), " ", OS.get_time())
 		OS.delay_msec(400)
 
-#		print("wait")
 #		semaphore.wait() #wait for a new message to be posted in the queue
-#		print("post")
 		
 		#exit ?
 		exit_lock.lock()
@@ -350,17 +340,17 @@ func _loading_process(dummy):
 				ACTION_FREE_INSTANCE:
 					_process_free_instance_action(action.zone_id)
 		
-	print("loading process stopped")
+	_print("loading process stopped")
 		
 func request_load(zone_id, priority = false):
 	
-	print("request load zone ", zone_id)
+	_print(str("request load zone ", zone_id))
 	
 	loaded_zones_lock.lock()
 	
 	if loaded_zones.has(zone_id):
 		var resource = loaded_zones[zone_id].resource
-		print("zone ", zone_id, " already loaded")
+		_print(str("zone ", zone_id, " already loaded"))
 		loaded_zones_lock.unlock()
 		emit_signal("sig_zone_loading_finished", zone_id, resource)
 		return
@@ -371,37 +361,33 @@ func request_load(zone_id, priority = false):
 	
 func request_unload(zone_id):
 	
-	print("request unload zone ", zone_id)
+	_print(str("request unload zone ", zone_id))
 	
 	post_queue_action(ACTION_UNLOAD, zone_id, null)
 	
 func request_instance(zone_id, priority = false):
 	
-	print("request instance zone ", zone_id)
+	_print(str("request instance zone ", zone_id))
 	
 	post_queue_action(ACTION_INSTANCE, zone_id, priority)
 	
 func request_free_instance(zone_id):
 	
-	print("request free zone ", zone_id)
+	_print(str("request free zone ", zone_id))
 	
 	post_queue_action(ACTION_FREE_INSTANCE, zone_id)
 	
 func request_pruning(keep_zones):
-	
-	print("pruning start")
 	
 	loaded_zones_lock.lock()
 	
 	for zone in loaded_zones:
 
 		if not zone in keep_zones:
-			print("PRUNNING: request unloading ", zone)
+			_print(str("prunning: request unload ", zone))
 			request_unload(zone)
 	
 	loaded_zones_lock.unlock()
-	
-	print("prune finish")
 	
 func exit():
 	
